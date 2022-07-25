f8963e3369e91a6c17b7c01126e515f4e6e47b36
alexisherolv <alexisherolv@gmail.com>
2022-07-25 15:48:04 -0500

    Config

--- configs/config.js
+++ configs/config.js
@@ -13,54 +13,6 @@ async function getExpiration(){
     }
 }
 
-async function getExpirationDistances(){
-    try{
-        let pool = await sql.connect(config);
-        let generalParameters = await pool.request()
-            .input('pvOptionCRUD', sql.VarChar, "R")
-            .execute('spCat_General_Parameters_CRUD_Records')
-        return generalParameters.recordsets[0][13].Value
-    }catch(error){
-        console.log(error)
-    }
-}
-
-async function getExpiration69(){
-    try{
-        let pool = await sql.connect(config);
-        let generalParameters = await pool.request()
-            .input('pvOptionCRUD', sql.VarChar, "R")
-            .execute('spCat_General_Parameters_CRUD_Records')
-        return generalParameters.recordsets[0][12].Value
-    }catch(error){
-        console.log(error)
-    }
-}
-
-async function getSecret69(){
-    try{
-        let pool = await sql.connect(config);
-        let generalParameters = await pool.request()
-            .input('pvOptionCRUD', sql.VarChar, "R")
-            .execute('spCat_General_Parameters_CRUD_Records')
-        return generalParameters.recordsets[0][11].Value
-    }catch(error){
-        console.log(error)
-    }
-}
-
-async function getSecretDistances(){
-    try{
-        let pool = await sql.connect(config);
-        let generalParameters = await pool.request()
-            .input('pvOptionCRUD', sql.VarChar, "R")
-            .execute('spCat_General_Parameters_CRUD_Records')
-        return generalParameters.recordsets[0][14].Value
-    }catch(error){
-        console.log(error)
-    }
-}
-
 async function getSecret(){
     try{
         let pool = await sql.connect(config);
@@ -73,54 +25,7 @@ async function getSecret(){
     }
 }
 
-async function getGoogleApiKey(){
-    try{
-        let pool = await sql.connect(config);
-        let generalParameters = await pool.request()
-            .input('pvOptionCRUD', sql.VarChar, "R")
-            .execute('spCat_General_Parameters_CRUD_Records')
-        return generalParameters.recordsets[0][15].Value
-    }catch(error){
-        console.log(error)
-    }
-}
-
-// Secret - Expiration Timbrado
-
-async function getExpirationTimbrado(){
-    try{
-        let pool = await sql.connect(config);
-        let expiration = await pool.request()
-            .input('pvOptionCRUD', sql.VarChar, "R")
-            .input('piIdParameter', sql.Int, 18)
-            .execute('spCat_General_Parameters_CRUD_Records')
-        return expiration.recordset[0].Value
-    }catch(error){
-        console.log(error)
-    }
-}
-
-async function getSecretTimbrado(){
-    try{
-        let pool = await sql.connect(config);
-        let secret = await pool.request()
-            .input('pvOptionCRUD', sql.VarChar, "R")
-            .input('piIdParameter', sql.Int, 17)
-            .execute('spCat_General_Parameters_CRUD_Records')
-        return secret.recordset[0].Value
-    }catch(error){
-        console.log(error)
-    }
-}
-
 module.exports = {
     getExpiration: getExpiration,
     getSecret: getSecret,
-    getSecret69 : getSecret69,
-    getExpiration69 : getExpiration69,
-    getSecretDistances : getSecretDistances,
-    getExpirationDistances : getExpirationDistances,
-    getGoogleApiKey : getGoogleApiKey,
-    getExpirationTimbrado: getExpirationTimbrado,
-    getSecretTimbrado: getSecretTimbrado,
 }
